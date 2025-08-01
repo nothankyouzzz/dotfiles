77cacc414b360a7a0bb88a90d4fe9eee68806ac1
nothankyou <61371380+nothankyouzzz@users.noreply.github.com>
2025-08-02 00:54:28 +0800

    [neovim] fix(venv-selector): use forked lazyvim

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { dir = "~/sync/lua/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
   },
