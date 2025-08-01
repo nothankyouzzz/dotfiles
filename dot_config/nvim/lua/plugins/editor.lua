574e16efcda85a361a564a3f25606b67bfe1aa30
nothankyou <61371380+nothankyouzzz@users.noreply.github.com>
2025-08-01 16:46:08 +0800

    [neovim] add codecompanion's chat window to edgybar

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -24,8 +24,28 @@ return {
   {
     "folke/edgy.nvim",
     opts = {
+      right = {
+        {
+          title = "AI Chat",
+          ft = "codecompanion",
+          size = { width = 0.35 },
+          filter = function(_, win)
+            return vim.api.nvim_win_get_config(win).relative == ""
+          end,
+        },
+      },
       animate = { enabled = false },
     },
+  },
+
+  -- idk why it fixes the issue with neo-tree not opening files in the right window
+  -- logically speaking, lazyextra ui.edgy has included this configuration.
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.open_files_do_not_replace_types, "edgy")
+    end,
   },
 
   {
