fa09d478606174aad3d58e7d82c5b046834989d0
nothankyou <luxun070188@outlook.com>
2025-08-02 23:13:41 +0800

    [neovim] remove edgy.nvim workaround
    because I switch to my LazyVim fork.

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -16,16 +16,6 @@ return {
     },
   },
 
-  -- extra ui.edgy include configuration for fixing confliction neo-tree and starter
-  -- but it did not work, so I just use the efficacious part here.
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    optional = true,
-    opts = function(_, opts)
-      table.insert(opts.open_files_do_not_replace_types, "edgy")
-    end,
-  },
-
   {
     "folke/snacks.nvim",
     opts = {
