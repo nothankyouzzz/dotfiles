38519dfd1562a4930a814d9079444aea052dea34
nothankyou <61371380+nothankyouzzz@users.noreply.github.com>
2025-09-06 11:42:17 +0800

    [neovim] use multiple distro with NVIM_APPNAME

---
+++ dot_config/nvim-lazy/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.autoformat = false
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.codecompanion_yolo_mode = true
+
+if vim.g.neovide then
+  vim.o.guifont = "Cascadia Code NF:h13"
+end
