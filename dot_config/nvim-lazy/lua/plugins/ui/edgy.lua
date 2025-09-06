88598cb07a5b56a94ff11fd2737f4a7b0479e07c
nothankyou <luxun070188@outlook.com>
2025-09-06 19:11:59 +0800

    [neovim] codecompanion & edgy
    codecompanion:
    
    - spinner
    - custom header
    
    edgy:
    
    - adaptive resize

--- dot_config/nvim-lazy/lua/plugins/ui/edgy.lua
+++ dot_config/nvim-lazy/lua/plugins/ui/edgy.lua
@@ -1,8 +1,80 @@
+---@return boolean
+local function at_top_edge()
+  return vim.fn.winnr() == vim.fn.winnr("k")
+end
+
+---@return boolean
+local function at_bottom_edge()
+  return vim.fn.winnr() == vim.fn.winnr("j")
+end
+
+---@return boolean
+local function at_left_edge()
+  return vim.fn.winnr() == vim.fn.winnr("h")
+end
+
+---@return boolean
+local function at_right_edge()
+  return vim.fn.winnr() == vim.fn.winnr("l")
+end
+
+-- Assumption: a window cannot simultaneously be at
+-- all four edges (top, bottom, left, right)
+--
+---@param direction "left" | "right" | "up" | "down"
+---@param amount? number positive integer
+local function resize(win, direction, amount)
+  amount = amount or 2
+
+  local negate = (direction == "up" and at_top_edge())
+    or (direction == "down" and at_bottom_edge())
+    or (direction == "left" and at_left_edge())
+    or (direction == "right" and at_right_edge())
+
+  if negate then
+    amount = -amount
+  end
+
+  if direction == "up" or direction == "down" then
+    win:resize("height", amount)
+  elseif direction == "left" or direction == "right" then
+    win:resize("width", amount)
+  end
+end
+
 return {
   "folke/edgy.nvim",
   opts = {
     animate = {
       enabled = false,
+    },
+    right = {
+      {
+        title = "AI Chat",
+        ft = "codecompanion",
+        size = { width = 0.35 },
+        filter = function(_, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      },
+    },
+    keys = {
+      -- increase width
+      ["<c-Right>"] = function(win)
+        resize(win, "right")
+      end,
+      -- decrease width
+      ["<c-Left>"] = function(win)
+        resize(win, "left")
+      end,
+      -- increase height
+      ["<c-Up>"] = function(win)
+        resize(win, "up", 1)
+      end,
+      -- decrease height
+      ["<c-Down>"] = function(win)
+        resize(win, "down", 1)
+      end,
     },
   },
 }
