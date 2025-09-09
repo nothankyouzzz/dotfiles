d12e3f538d5eb570ac52a92a9183e66eaa3c7887
nothankyou <61371380+nothankyouzzz@users.noreply.github.com>
2025-09-09 14:54:45 +0800

    refactor(fish_title): simplify prompt logic and remove unused last_command argument
    - Removed the last_command argument from the fish_title function.
    - Simplified prompt construction by eliminating conditional logic.
    - Updated function description and added note about separation from
    pure-fish/pure.

--- dot_config/private_fish/functions/fish_title.fish
+++ dot_config/private_fish/functions/fish_title.fish
@@ -1,16 +1,11 @@
+# Separated from pure-fish/pure
 function fish_title \
-    --description "Set title to current folder and shell name" \
-    --argument-names last_command
+    --description "Set title to current folder and shell name"
 
     set --local current_folder (basename (pwd))
     set --local current_command (status current-command 2>/dev/null; or echo $_)[1]
     set --local separator "|"
 
-    set --local prompt "$current_folder: $last_command $separator $current_command"
-
-    if test -z "$last_command"
-        set prompt "$current_folder $separator $current_command"
-    end
-
+    set --local prompt "$current_folder $separator $current_command"
     echo $prompt
 end
