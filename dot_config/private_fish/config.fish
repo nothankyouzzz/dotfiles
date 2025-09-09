0e3bc1ff2511bd236d74327d78cd5f0c68dfd485
nothankyou <luxun070188@outlook.com>
2025-09-09 14:49:55 +0800

    refactor(fish): extract functions and cleanup config
    - Move `y`, `update`, and `fish_title` functions to separate files under
    `functions/`
    - Remove function definitions from `config.fish` for better modularity
    - Add comments and organize sections in `config.fish`
    - No functional changes to shell behavior

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,31 +1,17 @@
+# Aliases
 alias lvim="NVIM_APPNAME=nvim-lazy nvim"
 alias avim="NVIM_APPNAME=nvim-astro nvim"
 
+# Theme
 source ~/.config/fish/themes/tokyonight_moon.fish
 
-function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
+# Environment variables
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+set -gx PATH $HOME/.cabal/bin /home/nothankyou/.ghcup/bin $PATH # ghcup-env
 
-function update
-  sudo snap refresh
-  sudo apt update
-  sudo apt full-upgrade
-  sudo apt autoremove
-end
-
-# shell hooks
+# Shell hooks
 fzf --fish | source
 zoxide init fish | source
 conda "shell.fish" hook | source
 direnv hook fish | source
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-set -gx PATH $HOME/.cabal/bin /home/nothankyou/.ghcup/bin $PATH # ghcup-env
-
 starship init fish | source
