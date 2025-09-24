00d4eb978c4f17dabd62b77027ee0223cdac5d38
nothankyou <luxun070188@outlook.com>
2025-09-24 11:06:39 +0800

    feat(fish): source API keys file
    Source ~/.config/fish/api_keys.fish for local secrets.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,6 +5,9 @@ alias avim="NVIM_APPNAME=nvim-astro nvim"
 # Theme
 source ~/.config/fish/themes/tokyonight_moon.fish
 
+# API Keys
+source ~/.config/fish/api_keys.fish
+
 # Environment variables
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin /home/nothankyou/.ghcup/bin $PATH # ghcup-env
