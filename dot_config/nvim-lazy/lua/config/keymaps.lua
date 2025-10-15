6de01d0c02f89cd414e746d8e62b8cd41299e106
nothankyou <luxun070188@outlook.com>
2025-10-15 19:09:44 +0800

    refactor(keymaps): remove redundant keymap for clearing search highlight

--- dot_config/nvim-lazy/lua/config/keymaps.lua
+++ dot_config/nvim-lazy/lua/config/keymaps.lua
@@ -1,6 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<A-h>", function()
-  vim.cmd("nohl")
-end, { desc = "Clear Search Hightlight", silent = true })
