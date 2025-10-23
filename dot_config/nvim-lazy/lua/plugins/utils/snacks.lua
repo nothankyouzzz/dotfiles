1d94f2dda9d8e8994e6532a284e58fe4f605ab91
nothankyou <luxun070188@outlook.com>
2025-10-24 03:17:43 +0800

    feat(edgy): expose snacks layout in left pane

--- dot_config/nvim-lazy/lua/plugins/utils/snacks.lua
+++ dot_config/nvim-lazy/lua/plugins/utils/snacks.lua
@@ -1,8 +1,23 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    scroll = {
-      enabled = false,
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = {
+        enabled = false,
+      },
     },
+  },
+
+  {
+    "folke/edgy.nvim",
+    opts = function(_, opts)
+      opts.left = opts.left or {}
+      table.insert(opts.left, {
+        ft = "snacks_layout_box",
+        filter = function(_, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      })
+    end,
   },
 }
