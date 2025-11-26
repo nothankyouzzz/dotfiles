deea35377d9f59954b3bddb31340b3479c9c16ad
nothankyou <luxun070188@outlook.com>
2025-11-26 23:44:29 +0800

    refactor(nvim): let snacks govern its own explorer window size

--- dot_config/nvim-lazy/lua/plugins/utils/snacks.lua
+++ dot_config/nvim-lazy/lua/plugins/utils/snacks.lua
@@ -7,18 +7,4 @@ return {
       },
     },
   },
-
-  {
-    "folke/edgy.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.left = opts.left or {}
-      table.insert(opts.left, {
-        ft = "snacks_layout_box",
-        filter = function(_, win)
-          return vim.api.nvim_win_get_config(win).relative == ""
-        end,
-      })
-    end,
-  },
 }
